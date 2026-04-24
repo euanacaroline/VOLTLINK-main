75e838c97c5c699b9692a8783e43b4ff566c2d69
euanacaroline <acarolisbn@gmail.com>
2026-04-23 22:43:53 -0300

    Integrandi pagamento e banco de dados

--- core/pagamento.py
+++ core/pagamento.py
@@ -1,61 +1,91 @@
+import os
+import time
 from database.db_handler import db_singleton
 
-def add_card(user_id, method_type, card_name, card_number, card_expiry, cvv):
+def limpar_tela(): 
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def cadastrar_cartao(user_id, tipo_metodo): 
+    print(f"\n    CADASTRO DE NOVO CARTÃO  ({tipo_metodo.upper()})   ")
     """Valida e salva um novo cartão no banco de dados."""
-    if not card_name:
-        return (False, "Erro: O nome não pode estar vazio.")
-    if len(card_number) != 16 or not card_number.isdigit():
-        return (False, "Erro: Cartão inválido! Digite os 16 números.")
-    if "/" not in card_expiry or len(card_expiry) != 5:
-        return (False, "Erro: Formato de validade inválido (use MM/AA).")
+
+    nome_titular = input("Nome do Titular (como no cartão): ").strip().upper()
+    if not nome_titular: 
+        print(" Erro: O nome não pode estar vazio. ")
+        return None
+
+    numero_cartao = input("Número do cartão (16 dígitos):  ").replace(" ", " ") 
+    if len(numero_cartao) != 16 or not numero_cartao.isdigit():
+        print(" Erro: Cartão inválido! Digite os 16 números. ")
+        return None 
+    
+    validade = input("Validade (MM/AA): ").strip()
+    if "/" not in validade or len(validade) != 5: 
+        print(" Erro:: Formato de validade inválido (use MM/AA).")
+        return None 
+    
+    cvv = input("CVV (3 dígitos): ").strip()
     if len(cvv) != 3 or not cvv.isdigit():
-        return (False, "Erro: CVV inválido!")
+        print(" Erro: CVV inválido!")
+        return None 
+    
+    sucesso = cadastrar_cartao_no_banco(user_id, tipo_metodo, nome_titular, numero_cartao, validade)
+    if sucesso: 
+        print("\n ✅ Cartão salvo no banco de dados!")
+    else: 
+        print("n\ Erro ao salvar. ")
 
+
+def cadastrar_cartao_no_banco(user_id, tipo_metodo, nome_titular, numero_cartao, validade): 
     conn = db_singleton.get_connection()
-    if not conn: return (False, "Erro de banco de dados.")
+    if not conn: return (False, "Erro de conexão")
 
-    # Por segurança, guardamos apenas os 4 últimos dígitos do cartão
-    masked_number = "**** **** **** " + card_number[-4:]
-
-    try:
-        conn.execute("INSERT INTO payment_methods (user_id, method_type, card_name, card_number, card_expiry) VALUES (?, ?, ?, ?, ?)",
-                     (user_id, method_type, card_name, masked_number, card_expiry))
+    final_cartao = "**** **** ****" + numero_cartao[-4:]
+    try: 
+        conn.execute('''
+            INSERT INTO payment_methods (user_id, method_type, card_name, card_number, card_expiry)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (user_id, tipo_metodo, nome_titular, final_cartao, validade))
         conn.commit()
-        return (True, f"Cartão de {method_type} cadastrado com sucesso!")
-    except Exception as e:
-        return (False, f"Erro ao salvar cartão: {e}")
+        return True 
+    except Exception as e: 
+        print(f"Erro: {e}")
+        return False
+  
 
-def get_payment_methods(user_id):
-    """Retorna todas as formas de pagamento de um usuário."""
+def buscar_cartoes_usuario(id_usuario):
+    """Busca no banco todos os cartões que pertencem ao usuário logado"""
     conn = db_singleton.get_connection()
     if not conn: return []
-    return conn.execute("SELECT * FROM payment_methods WHERE user_id = ?", (user_id,)).fetchall()
+    return conn.execute("SELECT * FROM payment_methods WHERE user_id = ?", (id_usuario,)).fetchall()  #comando SELECT traz informações da tabela 
 
-def delete_payment_method(user_id, method_id):
+def excluir_cartao_banco(id_usuario, id_cartao):
     """Remove uma forma de pagamento."""
     conn = db_singleton.get_connection()
     try:
-        conn.execute("DELETE FROM payment_methods WHERE id = ? AND user_id = ?", (method_id, user_id))
+        '''Garante que o usuário apague um cartão que seja dele'''
+        conn.execute("DELETE FROM payment_methods WHERE id = ? AND user_id = ?", (id_cartao, id_usuario))
         conn.commit()
-        return (True, "Forma de pagamento removida com sucesso!")
+        return (True, "Cartão removido com sucesso!")
     except Exception as e:
         return (False, f"Erro ao remover: {e}")
 
-def update_card(user_id, method_id, card_name, card_number, card_expiry, cvv):
+def atualizar_dados_cartao(id_usuario, id_cartao, nome_titular, numero_cartao, validade, cvv):
     """Edita os dados de um cartão cadastrado."""
-    if not card_name:
+    if not nome_titular:
         return (False, "Erro: O nome não pode estar vazio.")
-    if len(card_number) != 16 or not card_number.isdigit():
+    if len(numero_cartao) != 16 or not numero_cartao.isdigit():
         return (False, "Erro: Cartão inválido! Digite os 16 números.")
-    if "/" not in card_expiry or len(card_expiry) != 5:
+    if "/" not in validade or len(validade) != 5:
         return (False, "Erro: Formato de validade inválido (use MM/AA).")
     if len(cvv) != 3 or not cvv.isdigit():
         return (False, "Erro: CVV inválido!")
 
     conn = db_singleton.get_connection()
-    masked_number = "**** **** **** " + card_number[-4:]
+    final_cartao = "**** **** **** " + numero_cartao[-4:]
     try:
-        conn.execute("UPDATE payment_methods SET card_name = ?, card_number = ?, card_expiry = ? WHERE id = ? AND user_id = ?", (card_name, masked_number, card_expiry, method_id, user_id))
+        '''Alterar dados nas colunas do banco de dados '''
+        conn.execute("UPDATE payment_methods SET card_name = ?, card_number = ?, card_expiry = ? WHERE id = ? AND user_id = ?", (nome_titular, final_cartao, validade, id_cartao, id_usuario))
         conn.commit()
         return (True, "Cartão atualizado com sucesso!")
     except Exception as e:
